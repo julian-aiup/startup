7f4bb23c1b3a39b94a3273318ce897785a2b7a66
Julian Aiup <julian.aiup@globant.com>
2016-10-11 11:23:05 -0300

    Julian Aiup Topic1 - Exercise 7.Create reusable function to perform AJAX calls

--- 01-JS-DOM-APIs/js/index.js
+++ 01-JS-DOM-APIs/js/index.js
@@ -3,20 +3,41 @@ function fade() {
     document.getElementById("fade-in").style.transition = "all 4s ease-out";
     document.getElementById("fade-in").style.opacity = 1;
   }, 3000);
-  console.log("HOLA");
 }
 
 document.getElementById("button-joke").addEventListener("click", function() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var xmlDoc = JSON.parse(this.responseText);
+  config = ["GET", "http://api.icndb.com/jokes/random", true];
+  var p1 = performAJAXCall(config);
+  // We define what to do when the promise is resolved/fulfilled with the then() call,
+  // and the catch() method defines what to do if the promise is rejected.
+  p1.then(
+    function(val) {
       var node = document.createElement("p");
-      var textNode = document.createTextNode(xmlDoc.value.joke);
+      var textNode = document.createTextNode(val.value.joke);
       node.appendChild(textNode);
       document.getElementById("fade-in").appendChild(node);
-    }
-  };
-  xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
-  xhttp.send();
+    })
+  .catch(
+    // Log the rejection reason
+    function(reason) {
+      console.log('Handle rejected promise ('+reason+') here.');
+    });
 });
+
+function performAJAXCall(config) {
+  var p1 = new Promise(
+    // The resolver function is called with the ability to resolve or
+    // reject the promise
+    function(resolve, reject) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          resolve(JSON.parse(this.responseText));
+        }
+      };
+      xhttp.open(config[0], config[1], config[2]);
+      xhttp.send();
+    }
+  );
+  return p1;
+}
