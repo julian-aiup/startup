fc2decbcb0def61fd7eaf1964a0f9b0e336f91dd
Julian Aiup <julian.aiup@globant.com>
2016-10-11 10:31:28 -0300

    Julian Aiup Topic1 - Exercise 6.Add click event to get XMLHttpRequest

--- 01-JS-DOM-APIs/js/index.js
+++ 01-JS-DOM-APIs/js/index.js
@@ -3,4 +3,20 @@ function fade() {
     document.getElementById("fade-in").style.transition = "all 4s ease-out";
     document.getElementById("fade-in").style.opacity = 1;
   }, 3000);
+  console.log("HOLA");
 }
+
+document.getElementById("button-joke").addEventListener("click", function() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var xmlDoc = JSON.parse(this.responseText);
+      var node = document.createElement("p");
+      var textNode = document.createTextNode(xmlDoc.value.joke);
+      node.appendChild(textNode);
+      document.getElementById("fade-in").appendChild(node);
+    }
+  };
+  xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+  xhttp.send();
+});
