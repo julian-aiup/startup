62da89bc28c24f9d0f10f3b8c82283c9ad284dd5
Julian Aiup <julian.aiup@globant.com>
2016-10-12 12:50:40 -0300

    Julian Aiup Topic1 - Exercise 12.Update makeTable function, takes bidimensional array and prints html table

--- 01-JS-DOM-APIs/js/table.js
+++ 01-JS-DOM-APIs/js/table.js
@@ -1,17 +1,23 @@
-function makeTable(numRows, numCols) {
+// arr must be bidimensional
+function makeTable(arr) {
   var tableNode = document.createElement("table");
   document.getElementById("table-div").appendChild(tableNode);
 
-  for (var r = 0; r < numRows; r++) {
+  for (var r = 0; r < arr.length; r++) {
     var rowNode = document.createElement("tr");
     tableNode.appendChild(rowNode);
-    for (var c = 0; c < numCols; c++) {
+    for (var c = 0; c < arr[r].length; c++) {
       var columnNode = document.createElement("th");
-      var textNode = document.createTextNode(r + " - " + c);
+      var textNode = document.createTextNode(arr[r][c]);
       columnNode.appendChild(textNode);
       rowNode.appendChild(columnNode);
     }
   }
 }
 
-makeTable(3,4);
+let arr = [];
+arr[0] = ["Hi", 51, 52];
+arr[1] = [60, "Bye", 62];
+arr[2] = ["The", 21, 12];
+
+makeTable(arr);
