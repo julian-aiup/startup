619cd88170e07c8c94b9481786cb96103ad2ae0f
Julian Aiup <julian.aiup@globant.com>
2016-10-13 12:24:55 -0300

    Julian Aiup Topic2 - Exercise 2.Instantiate some movies

--- 02-oop-inheritance/js/index.js
+++ 02-oop-inheritance/js/index.js
@@ -14,3 +14,14 @@ class Movie {
 
   }
 }
+
+let inception = new Movie("Inception", 2010, 148);
+let terminator = new Movie('Terminator', 1984, 90);
+console.log(inception instanceof Movie);
+console.log(inception instanceof Object);
+
+console.log(typeof Movie);
+console.log(typeof Movie.prototype.play);
+
+console.log(inception.title);
+console.log(terminator.title);
