ace8123c88dc197f23204d144256de40e8a2ec44
Julian Aiup <julian.aiup@globant.com>
2016-10-14 12:22:43 -0300

    Julian Aiup Topic2 - Exercise 3.Create EventEmitter class with on method

--- 02-oop-inheritance/js/index.js
+++ 02-oop-inheritance/js/index.js
@@ -4,15 +4,12 @@ class Movie {
     this.year = year;
     this.duration = duration;
   }
-  play() {
 
-  }
-  pause() {
+  play() { }
 
-  }
-  resume() {
+  pause() { }
 
-  }
+  resume() { }
 }
 
 let inception = new Movie("Inception", 2010, 148);
@@ -25,3 +22,40 @@ console.log(typeof Movie.prototype.play);
 
 console.log(inception.title);
 console.log(terminator.title);
+
+class EventEmitter {
+  constructor() {
+    this.events: [];
+  }
+
+  on(event, listener) {
+    let eventIndex = this.events.indexOf(event);
+    let eventFound;
+    let eventListenersLength;
+    if (eventIndex) {
+      eventFound = this.events[eventIndex];
+      eventFound.push(listener);
+    } else {
+      eventListenersLength = this.events.push(event);
+      this.events[eventListenersLength - 1].push(listener);
+    }
+  }
+
+  emit(event) {
+
+  }
+
+  off(event, listener) {
+
+  }
+}
+
+let eventEmitter = new EventEmitter();
+eventEmitter.on("click", function() {
+  alert("hello");
+});
+
+// Create an EventEmitter class with the following methods: on, emit, off.
+// The on method will allow to pass a callback or listener that will be executed each time a given event is triggered.
+// The emit method will allow a class to trigger events to be consumed by other functions/objects.
+// The off method will delete the listener.
