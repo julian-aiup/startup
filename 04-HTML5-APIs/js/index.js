ac964f41ea46cace4341f9a041369f5d1c10c841
Julian Aiup <julian.aiup@globant.com>
2016-10-20 12:53:51 -0300

    Julian Aiup Topic4 - Exercise 1.Create html with textarea and button, store with localStorage and IndexedDB textarea contet's

---
+++ 04-HTML5-APIs/js/index.js
@@ -0,0 +1,42 @@
+// Testing storage methods
+document.getElementById("save").addEventListener("click", function() {
+  let receivedComment = document.getElementById("comment").value;
+
+  // Testing localStorage
+  localStorage.setItem("comment", receivedComment);
+  console.log("Comment localStorage: " + localStorage["comment"]);
+
+  // Testing IndexedDB
+  // Open or create DB
+  let indexedDB = window.indexedDB;
+  let open = indexedDB.open("test", 1);
+
+  // Create the schema
+  open.onupgradeneeded = function() {
+    let db = open.result;
+    let store = db.createObjectStore("comments", {keyPath: "id"});
+    let index = store.createIndex("commentIndex", "comment");
+  };
+
+  open.onsuccess = function() {
+      // Start a new transaction
+      let db = open.result;
+      let tx = db.transaction("comments", "readwrite");
+      let store = tx.objectStore("comments");
+      let index = store.index("commentIndex");
+
+      // Add some data
+      store.put({id: 1, comment: receivedComment});
+
+      // Query the data
+      let getComment = store.get(1);
+      getComment.onsuccess = function() {
+        console.log("Comment IndexedDB: " + getComment.result.comment);
+      };
+
+      // Close the db when the transaction is done
+      tx.oncomplete = function() {
+          db.close();
+      };
+  }
+});
